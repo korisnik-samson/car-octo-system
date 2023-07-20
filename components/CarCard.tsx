e11ba4fd6771729a65f57ccb387ceb18b949a275
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-07-20 21:48:42 +0200

    [ADDED] Car Details Card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -20,7 +20,7 @@ const CarCard = ({ car }: CarCardProps) => {
     return (
         <div className="car-card group">
             <div className="car-card__content">
-                <h2>{make} {model}</h2>
+                <h2 className="car-card__content-title">{make} {model}</h2>
             </div>
             <p className="flex mt-6 text-[32px] font-extrabold">
                 <span className="self-start text-[14px] font-semibold">
