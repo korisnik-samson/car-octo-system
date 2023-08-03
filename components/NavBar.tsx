937aaf0d1e3288598cc3ffd2dfbfe1fdadb14ec1
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-08-03 14:54:30 +0200

    [MODIFIED] alt logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
         <header className="w-full absolute z-10">
             <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
                 <Link href="/" className="flex justify-center items-center">
-                    <Image src="/logo_2.svg" alt="Car Hub Logo" width={118} height={18} className="object-contain" />
+                    <Image src="/logo_2.svg" alt="Kingston Cars" width={118} height={18} className="object-contain" />
                 </Link>
                 <CustomButton title="Sign in" btnType="button"
                               containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
